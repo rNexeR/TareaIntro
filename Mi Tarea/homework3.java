98d694ade6cedc3aaf0aac0c770e7fefd8b514ad
manuel07 <manuelpc@unitec.edu>
2015-03-21 19:23:38 +0100

    Mi Tarea

--- Mi Tarea/homework3.java
+++ Mi Tarea/homework3.java
@@ -2,24 +2,28 @@
 import java.util.Scanner;
 public class homework3{
 	public static void main(String[] args) {
-		Scanner rd =  new Scanner(System.in);
+		Scanner rd = new Scanner(System.in);
+		//Definir variables
+        int num;
+        //Exigir un numero
+		System.out.print("Introduzca un numero: ");
+		num = rd.nextInt();
 
-		int x, y;
+		for(int y = 0;y<num;y++){//Establecer un contador que me sirva para la cantidad de asteriscos.
+			
+			for(int z = 0;z<y;z++){// Establecer otro contador para los espacios y que sea menor que el primer contador-
+				
+					System.out.print(" ");// para que me introduzca un espacio menos que la posicion en la que se encuentra.
+					
+					
 
-		System.out.print("Ingrese un numero: ");
-		x = rd.nextInt();
-        y = 0;
+				
+				
+				
+				
+			}
+			System.out.println("*");// Imprimir los asteriscos.
+		}
 
-        for(y=0;y<x;y++){
-				System.out.println("*");
-				for(z=0;z<y;z++){
-					System.out.println(" ");
-				}
-				if(y<x){
-					continue;
-				}else{
-					break;
-				}
-	    }
-    }
+	}
 }
