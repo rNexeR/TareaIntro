1dce55b48d8b66d8cab091d00eefb45c1f0ef171
rjcastell96 <robertocastellanos45@hotmail.com>
2015-03-23 07:07:23 -0600

    correccion de tarea

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,2 +1,19 @@
 //Realizar un programa que me imprima una cadena ingresada por el 
 //usuario, pero al reves. Por ejemplo: si ingreso Hola, me imprima aloH.
+import java.util.Scanner;
+public class Ejercicio4{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+
+		//aqui la persona ingresa una palabra
+		String palabra = scan.next();
+
+		//este for toma la lengitud de la palabra ingresada en lugar de sumar
+		//le resta desde su ultimo valor en el string para devolverla en reversa
+		for(int i = palabra.length()-1; i >= 0; i--){
+            System.out.println(palabra.charAt(i));
+        }
+        
+
+	}
+}
